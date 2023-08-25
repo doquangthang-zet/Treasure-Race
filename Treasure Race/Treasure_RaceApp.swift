ffaa2e6d3b1e32374518963cfd933520a1edf865
doquangthang-zet <zetsu2468@gmail.com>
2023-08-25 12:35:44 +0700

    Initial Commit

---
+++ Treasure Race/Treasure_RaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Treasure_RaceApp.swift
+//  Treasure Race
+//
+//  Created by Thang Do Quang on 25/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct Treasure_RaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
